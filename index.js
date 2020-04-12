b35df14276073bb36a37430cbf8597b3f58c8c0d
Angel Baro <angelbaro44@gmail.com>
2020-04-12 14:56:16 -0400

    test

--- index.js
+++ index.js
@@ -2,46 +2,74 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 console.log(canvas)
-
 var c = canvas.getContext('2d');
-let playerSize= 30;
-let x = 200;
-let y = 200;
-dx = 5;
-dy =5;
- // c.beginPath
- // c.fillStyle ='rgba(0,0,0,1)';
- // c.fillRect(x,y,100,100);
-// c.stroke()
-        
-function animate (){
- requestAnimationFrame(animate)
- c.clearRect(0,0,canvas.width,canvas.height); 
-  
-//  c.beginPath;
-//  c.arc(x,y,playerSize,0,Math.PI * 2,false); 
-//  c.fillStyle='rgba(200,0,0,1)';
-//  c.stroke();
 
- c.beginPath;
- c.fillStyle ='rgba(0,0,0,1)';
- c.fillRect(x,y,playerSize,playerSize);
- c.stroke();
-x+=dx;
-y+=dy;
-// console.log(x);
-console.log(y)
 
-if (x>= canvas.width - playerSize || x < 0){
-dx = -dx;
-strobeLight();
-}
-if (y >= canvas.height - playerSize || y < 0){
-    dy = -dy;
-    strobeLight();
+
+
+
+function Circle (x,y,dx,dy,playerSize,color){ 
+    this.x = x;
+    this.y = y;
+    this.dx = Math.random() * (dx * 3);
+    this.dy = Math.random() * (dy * 3)
+    this.playerSize = playerSize;
+    this.color = color
+
+    this.draw =function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.playerSize, 0, Math.PI*2);
+        c.fillStyle = this.color;
+        console.log(this.color)
+        c.fill();
+        c.closePath();
+
+    }
+    this.update = function (){
+        if (this.x + this.playerSize >= canvas.width || this.x - this.playerSize < 0){
+            this.dx = -this.dx;
+            // strobeLight();
+            }
+        if (this.y >= canvas.height - this.playerSize || this.y - this.playerSize < 0){
+                this.dy = - this.dy;
+                // strobeLight();
+                }
+            // this.x += this.dx;
+            // this.y += this.dy;
+
+            this.draw();
     }
 }
 
+
+var ballArray = [];
+
+for (let i = 0; i < 100; i++) {
+    let playerSize= 30;
+    let x = Math.random()*(canvas.width - playerSize*2)+playerSize;
+    let y = Math.random()*(canvas.height - playerSize*2)+playerSize;
+    let dx = 5;
+    let dy =5;
+   ballArray.push(new Circle(x,y,dx,dy,playerSize,getRndColor()));
+    console.log(ballArray)
+}
+
+function animate (){
+ requestAnimationFrame(animate)
+ c.clearRect(0,0,canvas.width,canvas.height);
+ for (let i = 0; i < ballArray.length; i++) {
+     ballArray[i].update();
+     
+ }
+}
+
+function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0,
+        o = Math.random() + .1;
+    return 'rgb(' + r + ',' + g + ',' + b + ','+o+')';
+}
 animate()
 
 
